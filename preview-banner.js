6d038b703d313fb449738746e282cc0fb0337cc7
azghamdi <amasalghamdi@gmail.com>
2026-08-18 09:56:06 +0300

    Add files via upload

--- preview-banner.js
+++ preview-banner.js
@@ -22,7 +22,7 @@
   if(!document.querySelector('link[href^="header-premium.css"]')){
     const headerStyle=document.createElement('link');
     headerStyle.rel='stylesheet';
-    headerStyle.href='header-premium.css?v=4';
+    headerStyle.href='header-premium.css?v=5';
     document.head.appendChild(headerStyle);
   }
   if(!document.querySelector('link[href^="gov-verification.css"]')){
